461a35774d4a30ef5e954bae6a61208817f3c4b4
deltanboi <pedroogheneochuko@gmail.com>
2022-01-28 13:04:04 +0100

    Rebuild carousel component
    Now installs styled-components automatically

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import babel from "rollup-plugin-babel";
 import resolve from "@rollup/plugin-node-resolve";
 import external from "rollup-plugin-peer-deps-external";
 import { terser } from "rollup-plugin-terser";
+import commonjs from "@rollup/plugin-commonjs";
 // import postcss from "rollup-plugin-postcss";
 
 export default [
@@ -23,6 +24,14 @@ export default [
       //   plugins: [],
       //   minimize: true,
       // }),
+      commonjs({
+        ignoreGlobal: true,
+        include: /\/node_modules\//,
+        namedExports: {
+          react: Object.keys(require("react")),
+          "react-is": Object.keys(require("react-is")),
+        },
+      }),
       babel({
         exclude: "node_modules/**",
         presets: ["@babel/preset-react"],
